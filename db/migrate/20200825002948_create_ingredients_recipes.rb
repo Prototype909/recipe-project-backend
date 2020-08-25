2004264df1164778ddc732aa634addb4d3444937
Steve Anderson <prototype909@msn.com>
2020-08-25 14:10:10 -0600

    added to migration table

--- db/migrate/20200825002948_create_ingredients_recipes.rb
+++ db/migrate/20200825002948_create_ingredients_recipes.rb
@@ -1,8 +1,8 @@
 class CreateIngredientsRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :ingredients_recipes do |t|
-      t.references :ingredient, null: false, foreign_key: true
-      t.references :recipe, null: false, foreign_key: true
+      t.references :ingredient, foreign_key: true
+      t.references :recipe, foreign_key: true
 
       t.timestamps
     end
