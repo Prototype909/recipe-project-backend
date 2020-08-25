6d47dd14477be8bb79fb228b4105d479b8350a8e
Steve Anderson <prototype909@msn.com>
2020-08-25 14:47:35 -0600

    build models and seed data

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   resources :ingredients
   resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/recipes' => 'recipes#index'
+  get '/recipes/:ingredient_name' => 'recipes#show'
+  post '/recipes' => 'recipes#create'
+
+  get '/ingredients' => 'ingredients#index'
 end
